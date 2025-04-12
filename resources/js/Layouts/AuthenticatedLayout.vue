a5d1c649db2fa6e638898e12f9084338053a47fb
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2025-04-12 18:16:16 +0300

    feat: remove notification and message svg

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -268,75 +268,12 @@ function search() {
             </form>
 
             <!-- Права часть меню -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
-              
-              <!-- Иконка уведомлений -->
-              <div class="relative notification-menu inline-flex items-center me-4">
-                <!-- Кнопка-иконка -->
-                <svg
-                  @click="toggleNotifications"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="cursor-pointer size-11 inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white focus:outline-none transition ease-in-out duration-150"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                  />
-                </svg>
-                <!-- Красная точка (счётчик непрочитанных) -->
-                <span
-                  v-if="unreadNotifications.length > 0"
-                  class="absolute top-0 right-0 w-2.5 h-2.5 bg-red-500 rounded-full"
-                ></span>
-                
-                <!-- Выпадающее меню уведомлений -->
-                <div
-                  v-show="isNotificationOpen"
-                  class="absolute right-6 mt-96 w-64 bg-white border border-gray-200 rounded-lg shadow-lg z-50
-                         transition-all duration-200 ease-in-out cursor-pointer"
-                  :class="{
-                    'opacity-100 pointer-events-auto scale-100': isNotificationOpen,
-                    'opacity-0 pointer-events-none scale-95': !isNotificationOpen
-                  }"
-                >
-                  <div v-if="notifications.length === 0" class="p-4 text-gray-500 text-sm">
-                    No new notifications
-                  </div>
-                  <ul v-else>
-                    <li
-                      v-for="notification in notifications.slice(0, 3)"
-                      :key="notification.id"
-                      class="px-4 py-2 hover:bg-gray-100 text-gray-700 text-sm"
-                    >
-                      <strong>Message:</strong> {{ notification.data.message }} <br/>
-                      <strong>Sender:</strong> {{ notification.data.sender_name }} <br/>
-                      <strong>Job Title:</strong> {{ notification.data.job_title || 'N/A' }}
-                    </li>
-                  </ul>
-                  <button
-                    @click="markAllAsRead"
-                    class="w-full text-center bg-red-500 text-white py-2 hover:bg-red-700"
-                  >
-                    Mark all as read
-                  </button>
-                </div>
-              </div>
-                            
-
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-11 inline-flex items-center py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white  focus:outline-none transition ease-in-out duration-150 " >
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-                              </svg>
-                            
-                              <template v-if="$page.props.auth.user">
-                                <NavLink id="my-balance" :href="route('balance.index')">
-                                    <p class="text-gray-500 mx-4"> ${{ $page.props.auth.user.balance }}</p>
-                                </NavLink>
-                              </template>
+            <div class="hidden sm:flex sm:items-center sm:ms-6">                            
+              <template v-if="$page.props.auth.user">
+                <NavLink id="my-balance" :href="route('balance.index')">
+                  <p class="text-gray-500 mx-4"> ${{ $page.props.auth.user.balance }}</p>
+                </NavLink>
+              </template>
 
                             <!-- Settings Dropdown -->
                             <div id="my-profile" class="ms-3 relative" v-if="$page.props.auth && $page.props.auth.user">
