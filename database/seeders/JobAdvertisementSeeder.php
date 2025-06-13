63a70631a7173870093af398cd7c23c8898e47a9
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2025-06-13 21:34:20 +0300

    feat: update job ads seeder

--- database/seeders/JobAdvertisementSeeder.php
+++ database/seeders/JobAdvertisementSeeder.php
@@ -2,31 +2,35 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\JobAdvertisement;
 use App\Models\User;
-use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
 
 class JobAdvertisementSeeder extends Seeder
 {
-    public function run(): void
-    {
-        $faker = Faker::create('en_US');
-        $users = User::inRandomOrder()->take(10)->get();
-        $statuses = ['pending', 'approved'];
+    /**
+     * Run the database seeds.
+     */
+        public function run(): void
+        {
+                $faker = Faker::create('en_US');
+                $users = User::inRandomOrder()->take(10)->get();
+                $statuses = ['active', 'closed', 'draft'];
 
-        foreach ($users as $user) {
-            $numAds = rand(1, 3);
-            for ($i = 0; $i < $numAds; $i++) {
-                JobAdvertisement::create([
-                    'Title' => $faker->jobTitle,
-                    'Description' => $faker->paragraph(4, true),
-                    'creator' => $user->name,
-                    'Examples' => $faker->sentence(6),
-                    'Price' => $faker->randomFloat(2, 100, 3000),
-                    'Status' => $faker->randomElement($statuses),
-                ]);
+
+                foreach ($users as $user) {
+                    $numAds = rand(1, 3);
+                    for ($i = 0; $i < $numAds; $i++) {
+                    JobAdvertisement::factory()->create([
+                        'Title' => $faker->jobTitle,
+                        'Description' => $faker->paragraph(4, true),
+                        'creator' => $user->name,
+                        'creator_id' => $user->id,
+                        'Examples' => $faker->sentence(6),
+                        'Price' => $faker->randomFloat(2, 100, 3000),
+                        'Status' => $faker->randomElement($statuses),
+                    ]);
+                }
             }
         }
-    }
 }
