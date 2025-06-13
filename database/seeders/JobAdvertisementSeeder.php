9b0ce4997641f7553bd4a25606c43b7c9fa1e132
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2025-06-13 21:19:35 +0300

    feat: update job ads seeder

--- database/seeders/JobAdvertisementSeeder.php
+++ database/seeders/JobAdvertisementSeeder.php
@@ -2,26 +2,29 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use App\Models\JobAdvertisement;
 use App\Models\User;
-use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class JobAdvertisementSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        // Получаем случайных пользователей
-        $users = User::inRandomOrder()->take(5)->get();
+        $faker = Faker::create('en_US');
+        $users = User::inRandomOrder()->take(10)->get();
 
-        // Создаем объявления для первых 5 пользователей
         foreach ($users as $user) {
-            JobAdvertisement::factory()->create([
-                'creator' => $user->name, // Имя создателя
-                'creator_id' => $user->id, // ID создателя
-            ]);
+            $numAds = rand(1, 3);
+            for ($i = 0; $i < $numAds; $i++) {
+                JobAdvertisement::create([
+                    'Title' => $faker->jobTitle,
+                    'Description' => $faker->paragraph(4, true),
+                    'creator' => $user->name,
+                    'Examples' => $faker->sentence(6),
+                    'Price' => $faker->randomFloat(2, 100, 3000),
+                ]);
+            }
         }
     }
 }
