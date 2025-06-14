e349169b7fd9976259add2e0597d1a2bff4c0a68
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2025-06-14 17:05:01 +0300

    feat: working pusger connection

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,15 +36,8 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-            'cluster' => env('PUSHER_APP_CLUSTER'), 
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http',
-                'encrypted' => false,
-                'useTLS' => false,
-            ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
             ],
         ],
 
